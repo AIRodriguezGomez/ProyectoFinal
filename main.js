d72de5a4f3ca5758ba3c7329d15498c3d0dcb258
AIRodriguezGomez <125589543+AIRodriguezGomez@users.noreply.github.com>
2023-11-26 18:42:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Bienvenido/a a mi pagina de Recetas");
+
+let elemento = document.querySelector(".enlace");
+elemento.addEventListener("click", function(){
+    alert("Pronto habrá mas contenido en este espacio");
+});
+
+
